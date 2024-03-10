49d9e4a9b6c9cd62735bb61c2e2b4f8a9480484e
Humoyun.Dev <humoyunbektursunniyazov@gmail.com>
2024-03-11 01:35:38 +0500

    main

--- config/settings.py
+++ config/settings.py
@@ -81,12 +81,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'TbIGSFiSsViroPVBfFtfgaOTTXlqfEtD',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '53621',
     }
 }
+
 
 
 # Password validation
